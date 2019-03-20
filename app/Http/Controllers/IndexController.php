4078d623d0ccced72c3c0c7e2aae933750a358d8
Smirnov Aleksandr <smalvad@yandex.ru>
2019-03-20 10:16:09 +0900

    Index without cache

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -21,28 +21,28 @@ class IndexController extends Controller
     public function index(Request $request)
     {
         // Кэшируем общее количество обьявлений
-        if (Cache::has('countOfAllAdverts')) {
+/*        if (Cache::has('countOfAllAdverts')) {
             $countOfAllAdvert = Cache::get('countOfAllAdverts');
         } else {
             $countOfAllAdvert = Csfd::get()->count();
 
             Cache::store('redis')->put('countOfAllAdverts', $countOfAllAdvert, 86400);
-        }
+        }*/
 
         // Кэшируем общее количество PDF газет
-        if (Cache::has('countOfAllPdfs')) {
+/*        if (Cache::has('countOfAllPdfs')) {
             $countOfAllPdfs = Cache::get('countOfAllPdfs');
         } else {
             $countOfAllPdfs = Pdf::get()->count();
 
             Cache::store('redis')->put('countOfAllPdfs', $countOfAllPdfs, 86400);
-        }
+        }*/
 
         return view('index', [
             'rates'             => Rate::all(),
             'paymentMethods'    => PaymentMethod::where('on_off', 1)->get(),
-            'countOfAllAdvert'  => $countOfAllAdvert,
-            'countOfAllPdfs'    => $countOfAllPdfs
+            'countOfAllAdvert'  => 1500000,
+            'countOfAllPdfs'    => 10000
         ]);
     }
 }
