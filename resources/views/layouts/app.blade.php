7a1411cb353df57071a5780bc3bbd7066a79ca3f
Smirnov Aleksandr <smalvad@yandex.ru>
2019-04-05 17:54:51 +0900

    Bitrix contact form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,9 @@
     <script src="{{ mix('js/manifest.js') }}" defer></script>
     <script src="{{ mix('js/vendor.js') }}" defer></script>
     <script src="{{ mix('js/app.js') }}" defer></script>
+
+    {{-- Bitrix форма --}}
+    @include('partials.bitrix-form')
 </body>
 
 </html>
